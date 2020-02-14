b9fb7d1e9835ab74235642392226d6db470a5e1f
SSyeon <tjdusdlfkrhd@naver.com>
2020-02-14 09:32:46 +0900

    splash

---
+++ app/src/main/java/kr/hs/emrim/tjdusdlfkrhd/internship/GuideActivity.java
@@ -0,0 +1,61 @@
+package kr.hs.emrim.tjdusdlfkrhd.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class GuideActivity extends AppCompatActivity {
+
+    int cnt = 0;
+    ImageView guideimg1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+
+        Button nextBtn = (Button) findViewById(R.id.nextBtn);
+        Button prevBtn = (Button) findViewById(R.id.prevBtn) ;
+
+        guideimg1 = (ImageView)findViewById(R.id.guideimg);
+
+        // 다음 버튼.
+        nextBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cnt++;
+                if(cnt> 2) {
+                    cnt = 2;
+                }
+                show();
+            }
+        });
+
+        // 이전 버튼.
+        prevBtn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cnt--;
+                if(cnt < 0) {
+                    cnt = 0;
+                }
+                show();
+            }
+        });
+    }
+
+    public void show() {
+        if(cnt == 0) {
+            guideimg1.setImageResource(R.drawable.guide1);
+        }
+        else if(cnt==1){
+            guideimg1.setImageResource(R.drawable.guide2);
+        }
+        else if(cnt==2) {
+            guideimg1.setImageResource(R.drawable.guide3);
+        }
+    }
+}
