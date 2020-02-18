ea72cc9bf9c711c517817d73167954fa304fcaa2
SSyeon <tjdusdlfkrhd@naver.com>
2020-02-19 03:44:47 +0900

    home xml

--- app/src/main/java/kr/hs/emrim/tjdusdlfkrhd/internship/MainActivity.java
+++ app/src/main/java/kr/hs/emrim/tjdusdlfkrhd/internship/MainActivity.java
@@ -20,20 +20,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_home);
 
-        Button btn = (Button)findViewById(R.id.signUp);
-
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(
-                        getApplicationContext(), // 현재 화면의 제어권자
-                        SignUpActivity.class); // 다음 넘어갈 클래스 지정
-                startActivity(intent); // 다음 화면으로 넘어간다
-            }
-
-
-        });
+//        Button btn = (Button)findViewById(R.id.signUp);
+//
+//        btn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(
+//                        getApplicationContext(), // 현재 화면의 제어권자
+//                        SignUpActivity.class); // 다음 넘어갈 클래스 지정
+//                startActivity(intent); // 다음 화면으로 넘어간다
+//            }
+//
+//
+//        });
     }
 }
