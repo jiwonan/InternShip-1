9e07b2419fc63cacbb4c3201739dda74c1ba9d2d
SSyeon <tjdusdlfkrhd@naver.com>
2020-02-18 13:57:04 +0900

    signUp, signIn xml (LinearLayout)

---
+++ app/src/main/java/kr/hs/emrim/tjdusdlfkrhd/internship/SignInActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emrim.tjdusdlfkrhd.internship;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+
+    }
+}
